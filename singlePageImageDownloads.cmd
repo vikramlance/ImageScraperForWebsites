d09077e12ce7260cdb239240e63bd38299a7600a
vikramlance <vikram.lance@gmail.com>
2016-07-31 00:23:19 -0500

    windows script for downloading images from single page

---
+++ singlePageImageDownloads.cmd
@@ -0,0 +1,10 @@
+ 
+ REM image-scraper -s <folder> <link> --formats jpg
+ REM image-scraper -s myjp3 !myLink! --formats jpg
+  REM download all the images on a page
+  image-scraper -s myImageFolder http://road.cc/content/forum/59367-pictures-your-bike --formats jpg
+  
+  REM delete all files which are less than size 20kb
+  FOR %%F IN (.\myImageFolder\*.jpg) DO ( IF %%~zF LSS 20000  DEL %%F )
+  
+  REM pause
